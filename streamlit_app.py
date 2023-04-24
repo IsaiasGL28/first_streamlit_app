360f8538a31b59a92a0da0066ee26f0c6202bedd
IsaiasGL28 <131718849+IsaiasGL28@users.noreply.github.com>
2023-04-24 17:18:57 -0600

    Update streamlit_app.py
    Filtered df to show

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,5 +5,6 @@ st.title('Fruits dataframe')
 fruits_df = pd.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
 fruits_df = fruits_df.set_index('Fruit')
 fruits_list = list(fruits_df.index)
-st.multiselect('Pick some fruits: ', fruits_list, fruits_list[:2])
-st.dataframe(fruits_df)
+fruits_selected = st.multiselect('Pick some fruits: ', fruits_list, fruits_list[:2])
+df_to_show = fruits_df.loc[fruits_selected]
+st.dataframe(df_to_show)
