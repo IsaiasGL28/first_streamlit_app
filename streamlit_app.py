197b9f847c8bb43c70339604dbb79f97c3eb73b8
IsaiasGL28 <131718849+IsaiasGL28@users.noreply.github.com>
2023-04-24 17:15:44 -0600

    Update streamlit_app.py
    Multiselect picker updated

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,5 +4,6 @@ import pandas as pd
 st.title('Fruits dataframe')
 fruits_df = pd.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
 fruits_df = fruits_df.set_index('Fruit')
-st.multiselect('Pick some fruits: ', list(fruits_df.index))
+fruits_list = list(fruits_df.index)
+st.multiselect('Pick some fruits: ', fruits_list, fruits_list[:2])
 st.dataframe(fruits_df)
