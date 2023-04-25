a4f119c00387605aafd1c90e23d42712d9c10eb4
IsaiasGL28 <131718849+IsaiasGL28@users.noreply.github.com>
2023-04-25 12:26:54 -0600

    Update streamlit_app.py
    Add fruityvice api call

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,27 @@
 import streamlit as st
 import pandas as pd
+import requests
 
 st.title('My Mom\'s New Healthy Diner')
+
+# ----------------- BREAKFAST FAVORITES -----------
 st.header('Breakfast Favorites')
 st.write(':bowl_with_spoon: Omega 3 & Blueberry Oatmeal')
+st.write(':green_salad: Kale, Spinach & Rocket Smoothie')
+st.write(':chicken: Hard-Boiled Free-Range Egg')
+st.write(':avocado::bread: Avocado Toast')
+
+# ----------------- FRUIT PICKER -------------------------------
+st.header(':banana::mango: Build Your Own Fruit Smoothie :kiwifruit::grapes:')
 fruits_df = pd.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
 fruits_df = fruits_df.set_index('Fruit')
 fruits_list = list(fruits_df.index)
 fruits_selected = st.multiselect('Pick some fruits: ', fruits_list, fruits_list[:2])
 df_to_show = fruits_df.loc[fruits_selected]
 st.dataframe(df_to_show)
+
+#-------------------- FRUITYVICE API ---------------------
+api_url = 'https://fruityvice.com/api/fruit/'
+json_response = requests.get(api_url+'kiwi').json()
+fruityvice_df = pd.json_normalize(json_response)
+st.dataframe(fruityvice_df)
